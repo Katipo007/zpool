26f6472a2a2d1d1e10d2138ec950278d619e3c46
Ben <ben@steadfast.co.nz>
2025-07-14 21:56:07 +1200

    Actually expose PoolUnmanaged

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 pub const Handle = @import("handle.zig").Handle;
+pub const PoolUnmanaged = @import("pool.zig").PoolUnmanaged;
 pub const Pool = @import("pool.zig").Pool;
 
 // ensure transitive closure of test coverage
 comptime {
     _ = Handle;
     _ = Pool;
+    _ = PoolUnmanaged;
 }
